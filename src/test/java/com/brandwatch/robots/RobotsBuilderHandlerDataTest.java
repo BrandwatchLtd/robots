f8beac4c1332b77997f0cb6c55907272e7f490a9
Hamish Morgan <hamish@brandwatch.com>
2014-10-03 15:27:24 +0100

    NOTICKET Add a parser handler that build a new robots domain object.

---
+++ src/test/java/com/brandwatch/robots/RobotsBuilderHandlerDataTest.java
@@ -0,0 +1,46 @@
+package com.brandwatch.robots;
+
+import com.brandwatch.robots.domain.Robots;
+import com.brandwatch.robots.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+@RunWith(Parameterized.class)
+public class RobotsBuilderHandlerDataTest extends AbstractDataTest {
+
+    private RobotsBuilderHandler handler;
+
+    public RobotsBuilderHandlerDataTest(String resourceName) {
+        super(resourceName);
+    }
+
+    @Before
+    public void setup() throws IOException {
+        handler = new RobotsBuilderHandler();
+    }
+
+    @Test
+    public void whenParse_thenNoExceptionThrown() throws IOException, ParseException {
+        robotsTxtParser.parse(handler);
+    }
+
+    @Test
+    public void whenParser_thenHandlerReturnsNonNull() throws IOException, ParseException {
+        robotsTxtParser.parse(handler);
+        assertThat(handler.get(), notNullValue());
+    }
+
+    @Test
+    public void whenParser_then() throws IOException, ParseException {
+        robotsTxtParser.parse(handler);
+        Robots robots = handler.get();
+        System.out.println(robots);
+    }
+}
