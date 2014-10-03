e5e9a1b0532991c26bb4c22318821121c708febd
Hamish Morgan <hamish@brandwatch.com>
2014-10-03 14:51:48 +0100

    NOTICKET Added a collection of real world robot.txt example for data driven testing

---
+++ src/test/java/com/brandwatch/robotstxt/AbstractDataTest.java
@@ -0,0 +1,53 @@
+package com.brandwatch.robotstxt;
+
+import com.brandwatch.robotstxt.parser.RobotsTxtParser;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Resources;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import static com.google.common.io.Resources.getResource;
+
+public abstract class AbstractDataTest {
+
+    private final String resourceName;
+
+    public AbstractDataTest(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    @Parameterized.Parameters(name = "{index}: {0}")
+    public static Iterable<Object[]> data() {
+        return ImmutableList.<Object[]>builder()
+                .add(new Object[]{"http_boards.dailymail.co.uk_robots.txt"})
+                .add(new Object[]{"https_www.drupal.org_robots.txt"})
+                .add(new Object[]{"http_www.brandwatch.com_robots.txt"})
+                .add(new Object[]{"http_www.dailymail.co.uk_robots.txt"})
+                .add(new Object[]{"http_www.google.com_robots.txt"})
+                .add(new Object[]{"https_twitter.com_robots.txt"})
+                .add(new Object[]{"http_facebook.com_robots.txt"})
+                .add(new Object[]{"obama_http_www.whitehouse.gov_robots.txt"})
+                .add(new Object[]{"bush_http_www.whitehouse.gov_robots.txt"})
+                .add(new Object[]{"http_www.last.fm_robots.txt"})
+                .add(new Object[]{"http_www.reddit.com_robots.txt"})
+                .build();
+    }
+
+    protected RobotsTxtParser robotsTxtParser;
+
+    @Before
+    public final void setupAbstractTest() throws IOException {
+        robotsTxtParser = new RobotsTxtParser(resourceReader(resourceName));
+    }
+
+    public static Reader resourceReader(String name) throws IOException {
+        return Resources.asCharSource(
+                getResource(AbstractDataTest.class, name), Charsets.UTF_8
+        ).openBufferedStream();
+    }
+
+}
---
+++ src/test/java/com/brandwatch/robotstxt/parser/RobotsTxtParserDataTest.java
@@ -0,0 +1,34 @@
+package com.brandwatch.robotstxt.parser;
+
+import com.brandwatch.robotstxt.AbstractDataTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(Parameterized.class)
+public class RobotsTxtParserDataTest extends AbstractDataTest {
+
+    public RobotsTxtParserDataTest(String resourceName) {
+        super(resourceName);
+    }
+
+    private RobotsTxtHandler handler;
+
+    @Before
+    public void setup() throws IOException {
+        handler = mock(RobotsTxtHandler.class);
+    }
+
+
+    @Test
+    public void whenParse_thenNoExceptionThrown() throws IOException, ParseException {
+        robotsTxtParser.parse(handler);
+    }
+
+
+}
