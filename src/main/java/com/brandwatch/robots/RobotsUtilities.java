40d50042dbd68daf3e346ef5c05ac44b2c1e4453
Hamish Morgan <hamish@brandwatch.com>
2014-10-07 11:39:05 +0100

    NOTICKET Added support for path expression matching

--- src/main/java/com/brandwatch/robots/RobotsUtilities.java
+++ src/main/java/com/brandwatch/robots/RobotsUtilities.java
@@ -2,14 +2,22 @@ package com.brandwatch.robots;
 
 import com.brandwatch.robots.domain.Group;
 import com.google.common.base.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
+import java.net.URI;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.regex.Pattern.compile;
+import static java.util.regex.Pattern.quote;
 
 public class RobotsUtilities {
+
+    private static final Logger log = LoggerFactory.getLogger(RobotsUtilities.class);
 
     @Nonnull
     public Optional<Group> getBestMatchingGroup(@Nonnull List<Group> groups, @Nonnull String agentString) {
@@ -59,4 +67,45 @@ public class RobotsUtilities {
             return -1;
         }
     }
+
+    public boolean isPathExpressionMatched(@Nonnull String pathExpression, @Nonnull URI uri) {
+        return isPathExpressionMatched(pathExpression, uri.getPath());
+    }
+
+    boolean isPathExpressionMatched(@Nonnull String pathExpression, @Nonnull String absolutePath) {
+        checkArgument(absolutePath.startsWith("/"), "path is not absolute");
+        return compilePathExpression(pathExpression).matcher(absolutePath).matches();
+    }
+
+    @Nonnull
+    Pattern compilePathExpression(@Nonnull String pathExpression) {
+        final StringBuilder regex = new StringBuilder().append("^");
+
+        switch (pathExpression.charAt(0)) {
+            default:
+                regex.append('/');
+            case '/':
+            case '*':
+        }
+
+        final int len = pathExpression.length();
+        int start = 0;
+        int end;
+        while ((end = pathExpression.indexOf('*', start)) != -1) {
+            regex.append(quote(pathExpression.substring(start, end)))
+                    .append(".*?");
+            start = end + 1;
+        }
+
+        if (pathExpression.charAt(len - 1) == '$') {
+            regex.append(quote(pathExpression.substring(start, len - 1)))
+                    .append('$');
+        } else {
+            regex.append(quote(pathExpression.substring(start, len)))
+                    .append(".*?");
+        }
+
+        return compile(regex.toString());
+    }
+
 }
--- src/test/java/com/brandwatch/robots/RobotsUtilitiesTest.java
+++ src/test/java/com/brandwatch/robots/RobotsUtilitiesTest.java
@@ -9,10 +9,12 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(Enclosed.class)
@@ -227,6 +229,179 @@ public class RobotsUtilitiesTest {
         public void givenLowerCaseCrawlerName_whenGetBestMatchingGroup_thenResultEqualsExpected() {
             Optional<Group> bestMatch = utilities.getBestMatchingGroup(groups, crawlerName.toLowerCase());
             assertThat(bestMatch.get(), equalTo(expectedGroup));
+        }
+
+    }
+
+
+    public static class IsPathPatternMatchedTest {
+
+        private RobotsUtilities utilities;
+
+        @Before
+        public final void setup() {
+            utilities = new RobotsUtilities();
+        }
+
+        @Test
+        public void givenMatchingArgs_whenIsPathPatternMatched_thenReturnsTrue() {
+            String pathPattern = "/";
+            String path = "/index.html";
+            boolean result = utilities.isPathExpressionMatched(pathPattern, path);
+            assertThat(result, is(true));
+        }
+
+        @Test
+        public void givenNonMatchingArgs_whenIsPathPatternMatched_thenReturnsFalse() {
+            String pathPattern = "/stuff";
+            String path = "/index.html";
+            boolean result = utilities.isPathExpressionMatched(pathPattern, path);
+            assertThat(result, is(false));
+        }
+
+        @Test
+        public void givenMatchingWildcardPattern_whenIsPathPatternMatched_thenReturnsTrue() {
+            String pathPattern = "/*";
+            String path = "/index.html";
+            boolean result = utilities.isPathExpressionMatched(pathPattern, path);
+            assertThat(result, is(true));
+        }
+
+        @Test(expected = NullPointerException.class)
+        public void givenNullPattern_whenIsPathPatternMatched_thenThrowsNPE() {
+            String pathPattern = null;
+            String path = "/index.html";
+            utilities.isPathExpressionMatched(pathPattern, path);
+        }
+
+        @Test(expected = NullPointerException.class)
+        public void givenNullPath_whenIsPathPatternMatched_thenThrowsNPE() {
+            String pathPattern = "/";
+            String path = null;
+            utilities.isPathExpressionMatched(pathPattern, path);
+        }
+
+
+        // missing prefix slash
+
+    }
+
+
+    @RunWith(Parameterized.class)
+    public static class IsPathPatternMatchedTestDataTests {
+
+        private final String pathPattern;
+        private final URI uri;
+        private final boolean expectedResult;
+
+        private RobotsUtilities utilities;
+
+        public IsPathPatternMatchedTestDataTests(String pathPattern, String path, boolean expectedResult) {
+            this.pathPattern = pathPattern;
+            this.uri = URI.create("http://www.example.com" + (path.startsWith("/") ? "" : "/") + path);
+            this.expectedResult = expectedResult;
+        }
+
+        @Parameterized.Parameters(name = "{index}: {0} {1} {2}")
+        public static Iterable<Object[]> data() {
+            return ImmutableList.<Object[]>builder()
+                    .add(new Object[]{"/*", "/path/to/index.html", true})
+                    .add(new Object[]{"/*", "/index.html", true})
+                    .add(new Object[]{"/*", "/?query", true})
+                    .add(new Object[]{"/*", "/#fragment", true})
+                    .add(new Object[]{"/*", "/", true})
+                    .add(new Object[]{"/*", "", true})
+                    .add(new Object[]{"*", "/path/to/index.html", true})
+                    .add(new Object[]{"*", "/index.html", true})
+                    .add(new Object[]{"*", "/?query", true})
+                    .add(new Object[]{"*", "/#fragment", true})
+                    .add(new Object[]{"*", "/", true})
+                    .add(new Object[]{"*", "", true})
+                    .add(new Object[]{"/index.html", "/index.html", true})
+                    .add(new Object[]{"index.html", "/index.html", true})
+                    .add(new Object[]{"index.html", "/path/to/index.html", false})
+                    .add(new Object[]{"/index.html", "/index.html?query=foo", true})
+                    .add(new Object[]{"/index.html", "/index.html#fragment", true})
+                    .add(new Object[]{"/index.html", "/index.html.php", true})
+                    .add(new Object[]{"/index.html$", "/index.html.php", false})
+                    .add(new Object[]{"/fish", "/fish", true})
+                    .add(new Object[]{"/fish", "/fish.html", true})
+                    .add(new Object[]{"/fish", "/fish.html", true})
+                    .add(new Object[]{"/fish", "/fish/salmon.html", true})
+                    .add(new Object[]{"/fish", "/fishheads", true})
+                    .add(new Object[]{"/fish", "/fishheads/yummy.html", true})
+                    .add(new Object[]{"/fish", "/fish.php?id=anything", true})
+                    .add(new Object[]{"/fish", "/Fish.asp", false})
+                    .add(new Object[]{"/fish", "/catfish", false})
+                    .add(new Object[]{"/fish", "/?id=fish", false})
+                    .add(new Object[]{"/fish*", "/fish", true})
+                    .add(new Object[]{"/fish*", "/fish.html", true})
+                    .add(new Object[]{"/fish*", "/fish.html", true})
+                    .add(new Object[]{"/fish*", "/fish/salmon.html", true})
+                    .add(new Object[]{"/fish*", "/fishheads", true})
+                    .add(new Object[]{"/fish*", "/fishheads/yummy.html", true})
+                    .add(new Object[]{"/fish*", "/fish.php?id=anything", true})
+                    .add(new Object[]{"/fish*", "/Fish.asp", false})
+                    .add(new Object[]{"/fish*", "/catfish", false})
+                    .add(new Object[]{"/fish*", "/?id=fish", false})
+                    .add(new Object[]{"/fish/", "/fish/?id=anything", true})
+                    .add(new Object[]{"/fish/", "/fish/", true})
+                    .add(new Object[]{"/fish/", "/fish/salmon.htm", true})
+                    .add(new Object[]{"/fish/", "/fish", false})
+                    .add(new Object[]{"/fish/", "/fish.html", false})
+                    .add(new Object[]{"/fish/", "/Fish/Salmon.asp", false})
+                    .add(new Object[]{"fish/", "/fish/?id=anything", true})
+                    .add(new Object[]{"fish/", "/fish/", true})
+                    .add(new Object[]{"fish/", "/fish/salmon.htm", true})
+                    .add(new Object[]{"fish/", "/fish", false})
+                    .add(new Object[]{"fish/", "/fish.html", false})
+                    .add(new Object[]{"fish/", "/Fish/Salmon.asp", false})
+                    .add(new Object[]{"/*.php", "/filename.php", true})
+                    .add(new Object[]{"/*.php", "/folder/filename.php", true})
+                    .add(new Object[]{"/*.php", "/folder/filename.php?parameters", true})
+                    .add(new Object[]{"/*.php", "/folder/any.php.file.html", true})
+                    .add(new Object[]{"/*.php", "/filename.php/", true})
+                    .add(new Object[]{"/*.php", "/", false})
+                    .add(new Object[]{"/*.php", "/windows.PHP", false})
+                    .add(new Object[]{"/fish*.php", "/fish.php", true})
+                    .add(new Object[]{"/fish*.php", "/fishheads/catfish.php?parameters", true})
+                    .add(new Object[]{"/fish*.php", "/Fish.PHP", false})
+                    .add(new Object[]{"/*/docs/*.html", "/a/docs/1.html", true})
+                    .add(new Object[]{"/*/docs/*.html", "/a/docs/2.html", true})
+                    .add(new Object[]{"/*/docs/*.html", "/b/docs/1.html", true})
+                    .add(new Object[]{"/*/docs/*.html", "/b/docs/2.html", true})
+                    .add(new Object[]{"/*/docs/*.html", "/b/docs/2.html.stuff", true})
+                    .add(new Object[]{"/*/docs/*.html", "/a/b/c/docs/1.html", true})
+                    .add(new Object[]{"/*/docs/*.html", "/docs/1.html", false})
+                    .add(new Object[]{"/*/docs/*.html", "/a/docs/1.php", false})
+                    .add(new Object[]{"/*/docs/*.html", "//docs/1.html", true})
+                    .add(new Object[]{"*/index.html$", "/index.html", true})
+                    .add(new Object[]{"/index.htm$", "/index.htm", true})
+                    .add(new Object[]{"/index.htm$", "/index.html", false})
+                    .add(new Object[]{"/p", "/page", true})
+                    .add(new Object[]{"/", "/page", true})
+                    .add(new Object[]{"/folder/", "/folder/page", true})
+                    .add(new Object[]{"/folder", "/folder/page", true})
+                    .add(new Object[]{"/page", "/page.htm", true})
+                    .add(new Object[]{"/*.htm", "/page.htm", true})
+                    .add(new Object[]{"/$", "/", true})
+                    .add(new Object[]{"/", "/", true})
+                    .add(new Object[]{"/$", "/page.htm", false})
+                    .add(new Object[]{"/", "/page.htm", true})
+                    .add(new Object[]{"/************************************", "/index.html", true})
+                    .add(new Object[]{"/*$", "/index.html", true})
+                    .build();
+        }
+
+        @Before
+        public final void setup() {
+            utilities = new RobotsUtilities();
+        }
+
+        @Test
+        public void whenIsPathPatternMatched_thenResultEqualsExpected() {
+            boolean result = utilities.isPathExpressionMatched(pathPattern, uri);
+            assertThat(result, equalTo(expectedResult));
         }
 
     }
