5280ecc11af616b19953f820c87a1b002b91403d
Hamish Morgan <hamish.morgan@gmail.com>
2015-03-29 12:25:39 +0100

    NT stopped missusing the array creation unility

--- cli/src/test/java/com/brandwatch/robots/cli/converters/CharsetConverterDataTest.java
+++ cli/src/test/java/com/brandwatch/robots/cli/converters/CharsetConverterDataTest.java
@@ -42,7 +42,6 @@ import org.junit.runners.Parameterized;
 
 import java.nio.charset.Charset;
 
-import static com.brandwatch.robots.cli.TestUtils.array;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -64,39 +63,39 @@ public class CharsetConverterDataTest {
     public static Iterable<Object[]> data() {
         return ImmutableList.<Object[]>builder()
 
-                .add(array("ISO-8859-1", Charsets.ISO_8859_1))
-                .add(array("iso-ir-100", Charsets.ISO_8859_1))
-                .add(array("csISOLatin1", Charsets.ISO_8859_1))
-                .add(array("latin1", Charsets.ISO_8859_1))
-                .add(array("l1", Charsets.ISO_8859_1))
-                .add(array("IBM819", Charsets.ISO_8859_1))
-                .add(array("CP819", Charsets.ISO_8859_1))
+                .add(new Object[]{"ISO-8859-1", Charsets.ISO_8859_1})
+                .add(new Object[]{"iso-ir-100", Charsets.ISO_8859_1})
+                .add(new Object[]{"csISOLatin1", Charsets.ISO_8859_1})
+                .add(new Object[]{"latin1", Charsets.ISO_8859_1})
+                .add(new Object[]{"l1", Charsets.ISO_8859_1})
+                .add(new Object[]{"IBM819", Charsets.ISO_8859_1})
+                .add(new Object[]{"CP819", Charsets.ISO_8859_1})
 
-                .add(array("ascii", Charsets.US_ASCII))
-                .add(array("ANSI_X3.4-1968", Charsets.US_ASCII))
-                .add(array("ISO_646.irv:1991", Charsets.US_ASCII))
-                .add(array("cp367", Charsets.US_ASCII))
-                .add(array("iso-ir-6", Charsets.US_ASCII))
-                .add(array("ASCII", Charsets.US_ASCII))
-                .add(array("us", Charsets.US_ASCII))
-                .add(array("csASCII", Charsets.US_ASCII))
-                .add(array("ANSI_X3.4-1986", Charsets.US_ASCII))
-                .add(array("ISO646-US", Charsets.US_ASCII))
-                .add(array("IBM367", Charsets.US_ASCII))
+                .add(new Object[]{"ascii", Charsets.US_ASCII})
+                .add(new Object[]{"ANSI_X3.4-1968", Charsets.US_ASCII})
+                .add(new Object[]{"ISO_646.irv:1991", Charsets.US_ASCII})
+                .add(new Object[]{"cp367", Charsets.US_ASCII})
+                .add(new Object[]{"iso-ir-6", Charsets.US_ASCII})
+                .add(new Object[]{"ASCII", Charsets.US_ASCII})
+                .add(new Object[]{"us", Charsets.US_ASCII})
+                .add(new Object[]{"csASCII", Charsets.US_ASCII})
+                .add(new Object[]{"ANSI_X3.4-1986", Charsets.US_ASCII})
+                .add(new Object[]{"ISO646-US", Charsets.US_ASCII})
+                .add(new Object[]{"IBM367", Charsets.US_ASCII})
 
-                .add(array("UTF8", Charsets.UTF_8))
-                .add(array("UTF-8", Charsets.UTF_8))
+                .add(new Object[]{"UTF8", Charsets.UTF_8})
+                .add(new Object[]{"UTF-8", Charsets.UTF_8})
 
-                .add(array("UTF_16", Charsets.UTF_16))
-                .add(array("UTF-16", Charsets.UTF_16))
-                .add(array("UTF16", Charsets.UTF_16))
-                .add(array("unicode", Charsets.UTF_16))
+                .add(new Object[]{"UTF_16", Charsets.UTF_16})
+                .add(new Object[]{"UTF-16", Charsets.UTF_16})
+                .add(new Object[]{"UTF16", Charsets.UTF_16})
+                .add(new Object[]{"unicode", Charsets.UTF_16})
 
-                .add(array("UTF_16BE", Charsets.UTF_16BE))
-                .add(array("UTF-16BE", Charsets.UTF_16BE))
+                .add(new Object[]{"UTF_16BE", Charsets.UTF_16BE})
+                .add(new Object[]{"UTF-16BE", Charsets.UTF_16BE})
 
-                .add(array("UTF_16LE", Charsets.UTF_16LE))
-                .add(array("UTF-16LE", Charsets.UTF_16LE))
+                .add(new Object[]{"UTF_16LE", Charsets.UTF_16LE})
+                .add(new Object[]{"UTF-16LE", Charsets.UTF_16LE})
 
                 .build();
     }
