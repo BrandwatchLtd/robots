760fc74a91e63dad2c1c91dbc99b6602b9af9f3a
Hamish Morgan <hamish.morgan@gmail.com>
2015-04-03 08:11:07 +0100

    NT Removed unused code

--- core/src/main/java/com/brandwatch/robots/TemporaryDisallow.java
+++ core/src/main/java/com/brandwatch/robots/TemporaryDisallow.java
@@ -39,18 +39,8 @@ public class TemporaryDisallow extends IOException {
 
     private static final long serialVersionUID = -4504038118861545829L;
 
-    public TemporaryDisallow() {
-    }
-
     public TemporaryDisallow(String s) {
         super(s);
     }
 
-    public TemporaryDisallow(String s, Throwable throwable) {
-        super(s, throwable);
-    }
-
-    public TemporaryDisallow(Throwable throwable) {
-        super(throwable);
-    }
 }
