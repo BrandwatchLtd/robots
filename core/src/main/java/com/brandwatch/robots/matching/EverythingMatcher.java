1534ef8c759a2dffdd8f4f06e9d7bb82a0b0547f
Hamish Morgan <hamish.morgan@gmail.com>
2015-04-04 21:50:49 +0100

    Update inception year to 2014

--- core/src/main/java/com/brandwatch/robots/matching/EverythingMatcher.java
+++ core/src/main/java/com/brandwatch/robots/matching/EverythingMatcher.java
@@ -4,7 +4,7 @@ package com.brandwatch.robots.matching;
  * #%L
  * Robots (core)
  * %%
- * Copyright (C) 2015 Brandwatch
+ * Copyright (C) 2014 - 2015 Brandwatch
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
