c1503dfce04ddc4b4aa612268ff8e00806d884ec
bw-tom <tomh@brandwatch.com>
2021-01-18 17:37:48 +0000

    Remove Travis configuration

--- .travis_after_success.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# There's no point repeatedly updating coveralls, so do it after the jdk8 build only.
-if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]; then
-    echo "Running coveralls for JDK version: $TRAVIS_JDK_VERSION"
-    mvn cobertura:cobertura org.eluder.coveralls:coveralls-maven-plugin:report
-else
-    echo "Skipping coveralls for JDK version: $TRAVIS_JDK_VERSION"
-fi
